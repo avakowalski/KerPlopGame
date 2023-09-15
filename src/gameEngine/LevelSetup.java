b5b6cdcea601ad79e17ff8c1c9a447336fbc589d
Ava Kowalski <65259771+avakowalski@users.noreply.github.com>
2023-09-15 15:06:10 -0600

    adding new method to the level set up class

--- src/gameEngine/LevelSetup.java
+++ src/gameEngine/LevelSetup.java
@@ -1,5 +1,34 @@
 package gameEngine;
 
+import java.util.ArrayList;
+
+import levelPieces.GamePiece;
+
 public class LevelSetup {
+
+	public void createLevel(int levelNum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Drawable[] getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Moveable> getMovingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<GamePiece> getInteractingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPlayerStartLoc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
